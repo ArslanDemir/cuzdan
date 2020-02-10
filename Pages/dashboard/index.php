ead96b710581d3fc86100fe6ee835b6ff99053aa
arslan demir <isosaslan@gmail.com>
2020-02-10 18:51:48 +0300

    sayfa düzeni

--- Pages/dashboard/index.php
+++ Pages/dashboard/index.php
@@ -4,121 +4,174 @@
 	<title>Panel</title>
 </head>
 <body>
-<div class="container">
-	<div class="row" style="margin-top: 10px;">		
-		<div class="col">			
-			<div class="form-group">
-				<button class="form-control btn btn-danger" data-toggle="modal" data-target="#giderModal">Gider Ekle</button>
-			</div>
-		</div>
-		<div class="col">			
-			<div class="form-group">
-				<button class="form-control btn btn-success" data-toggle="modal" data-target="#gelirModal">Gelir Ekle</button>
-			</div>
-		</div>
-	</div>
+<div class="container-fluid" style="margin-top: 50px;">
 	<div class="row">
-		<div id="islemler" class="col-md-12">
-			<ul class="nav nav-tabs">
-				<li class="nav-item">
-					<a class="nav-link active" id="giderler-tab" data-toggle="tab" href="#giderler" role="tab" aria-controls="giderler" aria-selected="true">
-					Giderler
-					</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" id="gelirler-tab" data-toggle="tab" href="#gelirler" role="tab" aria-controls="gelirler" aria-selected="false">
-					Gelirler
-					</a>
-				</li>
-			</ul>
-			<div class="tab-content" id="tabContent">
-				<div class="tab-pane fade show active" id="giderler" aria-labelledby="giderler-tab">
-				</div>
-				<div class="tab-pane fade" id="gelirler" aria-labelledby="gelirler-tab">
-				</div>
+		<div class="col-md-2">
+			<div class="list-group">
+			  <span class="list-group-item list-group-item-action">İşlemler</span>
+			  <span class="list-group-item list-group-item-action" data-toggle="modal" data-target="#giderModal">Gider Ekle</span>
+			  <span class="list-group-item list-group-item-action" data-toggle="modal" data-target="#gelirModal">Gelir Ekle</span>
+			  <span class="list-group-item list-group-item-action" data-toggle="modal" data-target="#giderTuruModal" >Gider Türü Ekle</span>
+			  <span class="list-group-item list-group-item-action" data-toggle="modal" data-target="#gelirTuruModal">Gelir Türü Ekle</span>
 			</div>
 		</div>
-	</div>
-	<div id="modal-giderler">
-		<form action="" method="POST">
-		<div class="modal fade" id="giderModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title" id="exampleModalLabel"><span id="giderTarih"><?php echo date('d-m-Y');?></span>/  Gider Ekle</h5>
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div class="modal-body">
-							<div class="form-group">
-			        			<label for="giderBaslik	">Gider Başlığı</label>
-			        			<input type="text" id="giderBaslik" name="giderBaslik" class="form-control">
-			        		</div>
-			        		<div class="form-group">
-			        			<label for="giderTur">Gider Türü</label>
-			        			<select name="giderTur" id="giderTur" class="form-control">
-			        				<option value="1">Alışveriş</option>
-			        				<option value="2">Otogaz</option>
-			        				<option value="3">Fatura</option>
-			        			</select>
-			        		</div>
-			        		<div class="form-group">
-			        			<label for="giderMiktar">Miktar</label>
-			        			<input type="number" id="giderMiktar" name="giderMiktar" class="form-control">
-			        		</div>
-			        		<div class="form-group">
-			        			<label for="giderAciklama">Açıklama</label>
-			        			<textarea name="giderAciklama" id="giderAciklama" class="form-control"></textarea>
-			        		</div>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary" data-dismiss="modal">İptal</button>
-						<button type="button" class="btn btn-success" onclick="addExpenses()">Gideri Ekle</button>
+		<div class="col-md-10">
+			<div class="row">
+				<div id="islemler" class="col-md-12">
+					<ul class="nav nav-tabs">
+						<li class="nav-item">
+							<a class="nav-link active" id="giderler-tab" data-toggle="tab" href="#giderler" role="tab" aria-controls="giderler" aria-selected="true">
+							Giderler
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" id="gelirler-tab" data-toggle="tab" href="#gelirler" role="tab" aria-controls="gelirler" aria-selected="false">
+							Gelirler
+							</a>
+						</li>
+					</ul>
+					<div class="tab-content" id="tabContent">
+						<div class="tab-pane fade show active" id="giderler" aria-labelledby="giderler-tab">
+						</div>
+						<div class="tab-pane fade" id="gelirler" aria-labelledby="gelirler-tab">
+						</div>
 					</div>
 				</div>
 			</div>
-			</form>
-		</div>
-	</div>
-	<div id="modal-gelirler">
-		<form action="" method="POST">
-		<div class="modal fade" id="gelirModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title" id="exampleModalLabel"><span id="gelirTarih"><?php echo date('d-m-Y');?></span>/  Gelir Ekle</h5>
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
+			<div id="modal-giderler">
+				<form action="" method="POST">
+				<div class="modal fade" id="giderModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel"><span id="giderTarih"><?php echo date('d-m-Y');?></span>/  Gider Ekle</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+									<div class="form-group">
+					        			<label for="giderBaslik	">Gider Başlığı</label>
+					        			<input type="text" id="giderBaslik" name="giderBaslik" class="form-control">
+					        		</div>
+					        		<div class="form-group">
+					        			<label for="giderTur">Gider Türü</label>
+					        			<select name="giderTur" id="giderTur" class="form-control">
+					        				<option value="1">Alışveriş</option>
+					        				<option value="2">Otogaz</option>
+					        				<option value="3">Fatura</option>
+					        			</select>
+					        		</div>
+					        		<div class="form-group">
+					        			<label for="giderMiktar">Miktar</label>
+					        			<input type="number" id="giderMiktar" name="giderMiktar" class="form-control">
+					        		</div>
+					        		<div class="form-group">
+					        			<label for="giderAciklama">Açıklama</label>
+					        			<textarea name="giderAciklama" id="giderAciklama" class="form-control"></textarea>
+					        		</div>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">İptal</button>
+								<button type="button" class="btn btn-success" onclick="addExpenses()">Gideri Ekle</button>
+							</div>
+						</div>
 					</div>
-					<div class="modal-body">
-			        		<div class="form-group">
-			        			<label for="giderBaslik	">Gelir Başlığı</label>
-			        			<input type="text" id="gelirBaslik" name="gelirBaslik" class="form-control">
-			        		</div>
-			        		<div class="form-group">
-			        			<label for="gelirTur">Gelir Türü</label>
-			        			<select name="gelirTur" id="gelirTur" class="form-control">
-			        				<option value="4">Maaş</option>
-			        			</select>
-			        		</div>
-			        		<div class="form-group">
-			        			<label for="gelirMiktar">Miktar</label>
-			        			<input type="number" id="gelirMiktar" name="gelirMiktar" class="form-control">
-			        		</div>
-			        		<div class="form-group">
-			        			<label for="gelirAciklama">Açıklama</label>
-			        			<textarea name="gelirAciklama" id="gelirAciklama" class="form-control"></textarea>
-			        		</div>
+					</form>
+				</div>
+			</div>
+			<div id="modal-gelirler">
+				<form action="" method="POST">
+				<div class="modal fade" id="gelirModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel"><span id="gelirTarih"><?php echo date('d-m-Y');?></span>/  Gelir Ekle</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+					        		<div class="form-group">
+					        			<label for="giderBaslik	">Gelir Başlığı</label>
+					        			<input type="text" id="gelirBaslik" name="gelirBaslik" class="form-control">
+					        		</div>
+					        		<div class="form-group">
+					        			<label for="gelirTur">Gelir Türü</label>
+					        			<select name="gelirTur" id="gelirTur" class="form-control">
+					        				<option value="4">Maaş</option>
+					        			</select>
+					        		</div>
+					        		<div class="form-group">
+					        			<label for="gelirMiktar">Miktar</label>
+					        			<input type="number" id="gelirMiktar" name="gelirMiktar" class="form-control">
+					        		</div>
+					        		<div class="form-group">
+					        			<label for="gelirAciklama">Açıklama</label>
+					        			<textarea name="gelirAciklama" id="gelirAciklama" class="form-control"></textarea>
+					        		</div>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">İptal</button>
+								<button type="button" class="btn btn-success" onclick="addIncomes()">Geliri Ekle</button>
+							</div>
+						</div>
 					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary" data-dismiss="modal">İptal</button>
-						<button type="button" class="btn btn-success" onclick="addIncomes()">Geliri Ekle</button>
+				</form>
+				</div>
+			</div>
+			<div id="modal-gelir-turu">
+				<div class="modal fade" id="gelirTuruModal" tabindex="-1" role="dialog" aria-hidden="true">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel"><span id="gelirTarih"><?=$_SESSION['name']?></span>/  Gelir Türü Ekle</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+				        		<div class="form-group">
+				        			<label for="gelirTypeBaslik	">Gelir Türü Başlığı</label>
+				        			<input type="text" id="gelirTypeBaslik" name="gelirTypeBaslik" class="form-control">
+				        			<input type="hidden" id="gelirType" name="gelirType" value="0">
+				        			<input type="hidden" id="gelirTypeOwner" name="gelirTypeOwner" value="<?=$_SESSION['id']?>">
+				        		</div>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">İptal</button>
+								<button type="button" class="btn btn-success" onclick="addIncomesType()">Gelir Türünü Ekle</button>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</form>
+			<div id="modal-gider-turu">
+				<div class="modal fade" id="giderTuruModal" tabindex="-1" role="dialog" aria-hidden="true">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel"><span id="gelirTarih"><?=$_SESSION['name']?></span>/  Gider Türü Ekle</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+				        		<div class="form-group">
+				        			<label for="giderBaslik	">Gider Türü Başlığı</label>
+				        			<input type="text" id="gelirTypeBaslik" name="giderTypeBaslik" class="form-control">
+				        			<input type="hidden" id="giderType" name="giderType" value="1">
+				        			<input type="hidden" id="giderTypeOwner" name="giderTypeOwner" value="<?=$_SESSION['id']?>">
+				        		</div>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">İptal</button>
+								<button type="button" class="btn btn-success" onclick="addExpensesType()">Gider Türünü Ekle</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
